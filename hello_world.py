e6dd26c0d5a1bb816c5ada4dab3ec5fc1c8ae14d
dimonenok555 <dimonenok555@yandex.ru>
2025-11-17 17:02:18 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,4 @@ def main ()
 name: input ( Dmitry)
 print (f"Привет (Dmitry) Добро пожаловать в мир нейросетей!")
 if__name__== "__main__":main()
+git commit-m "Добавлены базовые файлы :hello _world  и environment
